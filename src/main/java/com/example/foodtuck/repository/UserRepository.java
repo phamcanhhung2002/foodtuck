7138a38e642dcf318f5fd02484fe57b497605862
Hung Pham <canhhungroon@gmail.com>
2024-12-14 15:27:23 +0700

    Add User entity

---
+++ src/main/java/com/example/foodtuck/repository/UserRepository.java
@@ -0,0 +1,15 @@
+package com.example.foodtuck.repository;
+
+import com.example.foodtuck.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByEmail(String email);
+
+    Optional<User> findByActivationCode(String code);
+}
